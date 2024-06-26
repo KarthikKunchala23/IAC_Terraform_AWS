628a51be0cd1ae59abaf2569527fd38bd8c111d8
karthik <karthikkunchala170.com>
2024-06-26 23:07:23 +0530

    fixed the for loop for private ip and private instance id

--- EC2_SG/ec2_instance_outputs.tf
+++ EC2_SG/ec2_instance_outputs.tf
@@ -10,10 +10,10 @@ output "public_instance_ip" {
 
 output "private_instance_id" {
   description = "private instance id's"
-  value = [for ec2private in module.ec2module.ec2-instance-private: ec2private.id]
+  value = [for ec2private in module.ec2-instance-private: ec2private.id]
 }
 
 output "private_ip" {
   description = "private ip's of private ec2 instances"
-  value = [for ec2private in module.module.ec2-instance-private: ec2private.private_ip]
+  value = [for ec2private in module.ec2-instance-private: ec2private.private_ip]
 }
