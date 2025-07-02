0fd3a5efa5409d37bf80080dbc9f69d7a354fbc1
karthik <karthikkunchala170.com>
2025-07-02 21:54:25 +0530

    added subnet manifest

---
+++ terraform/EKS/subnets.tf
@@ -0,0 +1,29 @@
+resource "aws_subnet" "az1" {
+  vpc_id            = var.vpc_id
+  cidr_block        = var.az1_cidr_block
+  availability_zone = var.az1_availability_zone
+  tags = {
+    Name = "eks-subnet-az1"
+  }
+  
+}
+
+resource "aws_subnet" "az2" {
+  vpc_id            = var.vpc_id
+  cidr_block        = var.az2_cidr_block
+  availability_zone = var.az2_availability_zone
+  tags = {
+    Name = "eks-subnet-az2"
+  }
+  
+}
+
+resource "aws_subnet" "az3" {
+  vpc_id            = var.vpc_id
+  cidr_block        = var.az3_cidr_block
+  availability_zone = var.az3_availability_zone
+  tags = {
+    Name = "eks-subnet-az3"
+  }
+  
+}
--- terraform/EKS/variables.tf
+++ terraform/EKS/variables.tf
@@ -9,3 +9,47 @@ variable "bootstrap_addons" {
   type        = bool
   default     = false
 }
+
+variable "vpc_id" {
+  description = "The ID of the VPC where the EKS cluster will be created"
+  type        = string
+  
+}
+
+variable "az1_cidr_block" {
+  description = "CIDR block for the first availability zone subnet"
+  type        = string
+  default     = "10.0.0.0/24"
+  
+}
+
+variable "az1_availability_zone" {
+  description = "The availability zone for the first subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "az2_cidr_block" {
+  description = "CIDR block for the second availability zone subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+  
+}
+
+variable "az2_availability_zone" {
+  description = "The availability zone for the second subnet"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "az3_cidr_block" {
+  description = "CIDR block for the third availability zone subnet"
+  type        = string
+  default     = "10.1.0.0/24"
+}
+
+variable "az3_availability_zone" {
+  description = "The availability zone for the third subnet"
+  type        = string
+  default     = "us-east-1a"
+}
