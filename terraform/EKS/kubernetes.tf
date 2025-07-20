0263664bf6f67e3418b09b54ccb6ce7fcd55a099
karthik <karthikkunchala170.com>
2025-07-20 16:57:24 +0530

    added proper cluster access

--- terraform/EKS/kubernetes.tf
+++ terraform/EKS/kubernetes.tf
@@ -1,7 +1,7 @@
 provider "kubernetes" {
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = aws_eks_cluster_auth.eks.token
+  host                   = aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
   
 }
 
--- terraform/EKS/main.tf
+++ terraform/EKS/main.tf
@@ -51,6 +51,11 @@ resource "aws_eks_cluster" "cluster" {
   ]
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.cluster.name
+}
+
+
 resource "aws_eks_access_entry" "admin_access" {
   cluster_name = aws_eks_cluster.cluster.name
   principal_arn = "arn:aws:iam::897722700244:user/karthik"
