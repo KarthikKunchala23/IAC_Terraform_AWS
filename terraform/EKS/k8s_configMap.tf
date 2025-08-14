3dbc7107e4ce04c35095935a8eb6c17d67d5b177
Karthik Kunchala <126945658+KarthikKunchala23@users.noreply.github.com>
2025-08-14 16:06:25 +0530

    Feature/eks iac: Manifest lifecycle added to kubernetes manifest (#24)
    * added karpenter manifests
    
    * modified code for karpenter
    
    * added aws kubeconfig job in workflow
    
    * Added new kubernetes manifest for aws-auth error
    
    * removed kubeconfig path
    
    ---------
    
    Co-authored-by: karthik <karthikkunchala170.com>

--- terraform/EKS/k8s_configMap.tf
+++ terraform/EKS/k8s_configMap.tf
@@ -1,23 +1,34 @@
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
+resource "kubernetes_manifest" "aws_auth" {
+  manifest = {
+    apiVersion = "v1"
+    kind       = "ConfigMap"
+    metadata = {
+      name      = "aws-auth"
+      namespace = "kube-system"
+    }
+    data = {
+      mapRoles = yamlencode([
+        {
+          rolearn  = aws_iam_role.eks_node_group.arn
+          username = "system:node:{{EC2PrivateDNSName}}"
+          groups   = ["system:bootstrappers", "system:nodes"]
+        },
+        {
+          rolearn  = aws_iam_role.eks_admin_role.arn
+          username = "eks-admin"
+          groups   = ["system:masters"]
+        }
+      ])
+    }
   }
 
-  data = {
-    mapRoles = yamlencode([
-      {
-        rolearn  = aws_iam_role.eks_node_group.arn
-        username = "system:node:{{EC2PrivateDNSName}}"
-        groups   = ["system:bootstrappers", "system:nodes"]
-      },
-      {
-        rolearn  = aws_iam_role.eks_admin_role.arn
-        username = "eks-admin"
-        groups   = ["system:masters"]
-      }
-    ])
+  lifecycle {
+    ignore_changes = [manifest]
   }
 
-  depends_on = [aws_eks_cluster.eks, aws_iam_role.eks_node_group, aws_iam_role.eks_admin_role]
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_iam_role.eks_node_group,
+    aws_iam_role.eks_admin_role
+  ]
 }
--- terraform/EKS/provider.tf
+++ terraform/EKS/provider.tf
@@ -3,7 +3,6 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"  # Adjust the path to your kubeconfig file
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
