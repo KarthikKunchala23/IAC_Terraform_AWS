2b1963f8e5771fc45bf9e8f32047c9300e52447f
karthik <karthikkunchala170.com>
2025-08-31 16:36:17 +0530

    removed the kube config path

--- terraform/karpenter_manifests/provider.tf
+++ terraform/karpenter_manifests/provider.tf
@@ -9,7 +9,6 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = ephemeral.aws_eks_cluster_auth.cluster_auth.token
