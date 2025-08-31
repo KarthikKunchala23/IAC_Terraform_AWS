d7380b0df3d2eedb47bd29c054f46c9ad2daaf34
karthik <karthikkunchala170.com>
2025-08-31 17:57:04 +0530

    version locked the terraform to 1.12.2

--- terraform/EKS/backend.tf
+++ terraform/EKS/backend.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = "=1.12.2"
   backend "s3" {
     bucket         = "my-terraform-state-bucket-example-for-eks-230798"
     key            = "eks-cluster/terraform.tfstate"
