8afcd75a5c685e455d81737bbe672d9ae7b7f4ec
karthik <karthikkunchala170.com>
2025-08-31 16:41:54 +0530

    removed emphemeral conifg

--- terraform/karpenter_manifests/provider.tf
+++ terraform/karpenter_manifests/provider.tf
@@ -11,14 +11,14 @@ terraform {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = ephemeral.aws_eks_cluster_auth.cluster_auth.token
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 
 provider "kubernetes-alpha" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = ephemeral.aws_eks_cluster_auth.cluster_auth.token
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 
